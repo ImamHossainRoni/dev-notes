df723cab896c9eb122f65ffcefb0e8089d88a4d2
Imam Hossain Roni <ironi@iquantile.com>
2022-03-18 11:41:25 +0600

    new changes added

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,6 +1,7 @@
 
 sudo docker stop jenkins-master
 sudo docker rm jenkins-master
-sudo docker run -p 8080:8080 --name=jenkins-master jenkins/jenkins
+# sudo docker run -p 8080:8080 --name=jenkins-master jenkins/jenkins
+sudo docker run -p 8080:8080 -p 50000:50000 --name=jenkins-master -d jenkins/jenkins
 
 
