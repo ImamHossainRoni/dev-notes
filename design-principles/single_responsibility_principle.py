42d90f84bd4c9c7f4dbecffe3fc29a59f16dbced
Imam Hossain Roni <ironi@iquantile.com>
2022-03-25 09:40:26 +0600

    Single Responsibility Principle applied

--- design-principles/single_responsibility_principle.py
+++ design-principles/single_responsibility_principle.py
@@ -1,3 +1,10 @@
+"""
+Single Responsibility Principle
+A class should have only one job.
+If a class has more than one responsibility, it becomes coupled.
+A change to one responsibility results to modification of the other responsibility.
+"""
+
 import json
 
 
@@ -21,6 +28,21 @@ class Author:
 author = Author('Uncle Bob')
 author.set_name('Uncle Bob')
 print(author.make_json())
+
+""" The above Author class violates the Single Responsibility Principle"""
+
+"""Let's organized the code according to  Single Responsibility Principle"""
+
+
+class Author:
+    def __init__(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
 
 
 class JsonMaker:
