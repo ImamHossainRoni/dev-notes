74ac0bb9c632229cb4ee2ab126de07e18ac1db6a
Imam Hossain Roni <95668704+roni-iq@users.noreply.github.com>
2022-05-26 08:45:08 +0600

    Update run.sh

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -7,5 +7,6 @@ sudo docker run -p 8080:8080 -p 50000:50000 --name=jenkins-master -d jenkins/jen
 # To execute jenkins in interactive shell
 # docker exec -it jenkins-master bash
 
+# To run existing jenkins docker image
+sudo docker run -p 8080:8080 -p5000:5000 -d jenkins/jenkins
 
-sudo cp ~/frontend/quantibly-dist/* .
