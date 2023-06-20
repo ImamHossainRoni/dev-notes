6b02f1d2c0bab5d52d0b13fda8d3449afeef6750
imamhossainroni <imamhossainroni95@gmail.com>
2023-06-20 15:50:11 +0600

    🚀Python higher order function that returns a function ✌️

--- python/higher-order-functions.py
+++ python/higher-order-functions.py
@@ -21,3 +21,19 @@ def voice(func):
 print(voice(loud))
 print(voice(quiet))
 
+# another example
+"""
+    dividend        divisor     quotient 
+          10    /     2      =     5
+"""
+
+
+def divisor(x):
+    def dividend(y):
+        return y / x
+
+    return dividend
+
+
+divide = divisor(2)
+print(divide(10))
