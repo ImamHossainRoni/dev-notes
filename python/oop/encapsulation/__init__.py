36a083a11d9f7ed8967ceea2151e865660412b4e
imamhossainroni <imamhossainroni95@gmail.com>
2023-06-20 19:33:27 +0600

    🚀Python Encapsulation introduced ✌️

---
+++ python/oop/encapsulation/__init__.py
@@ -0,0 +1,25 @@
+"""Encapsulation in Python is like putting things in a box. Imagine you have a box with a secret compartment inside.
+You can put your valuable items inside the secret compartment and close the box. Now, nobody can directly access or
+modify those items unless they know the special way to open the secret compartment.
+
+In programming, encapsulation is similar. You can create a class in Python, which acts as the box. The class holds
+your data (like variables) and methods (like functions) that operate on that data. By default, the data and methods
+inside the class are hidden from the outside world.
+
+To interact with the data and methods inside the class, you define special methods called getters and setters. These
+methods provide controlled access to the data. Just like knowing the special way to open the secret compartment in
+the box, you need to use the getters and setters to access or modify the data inside the class.
+
+Encapsulation has a few advantages:
+
+Protection: It helps protect your data from being accidentally changed or accessed inappropriately. Only the methods
+defined in the class can manipulate the data, ensuring its integrity.
+
+Simplicity: Encapsulation hides the complexity of how things work inside the class. Users of the class only need to
+know the public methods and their purposes. They don't need to understand the inner details of the implementation.
+
+Organization: Encapsulation allows you to organize your code better. You can group related data and methods together
+in a class, making it easier to manage and maintain your code.
+
+By using encapsulation, you can keep your data safe, make your code easier to understand, and have better control
+over how your objects are accessed and modified in Python."""
