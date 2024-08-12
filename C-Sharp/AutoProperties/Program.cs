57211d150f0bd450b806920ff004904de0768682
imamhossainroni <imamhossainroni95@gmail.com>
2024-08-13 00:39:36 +0600

    ✅ Simple Auto Property

--- C-Sharp/AutoProperties/Program.cs
+++ C-Sharp/AutoProperties/Program.cs
@@ -1,7 +1,21 @@
 ﻿using System;
 
 class Person
-{
-    public string FirstName { get; set; }
+{ 
+    public string Name { get; set; }
     
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person("Imam Hossain Roni");
+        Console.WriteLine($"Person's name is : {person.Name}");
+        
+    }
 }
