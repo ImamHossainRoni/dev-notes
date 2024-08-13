a04e66a9b2d8f3560b351526755d35ca561bdd38
imamhossainroni <imamhossainroni95@gmail.com>
2024-08-13 22:53:53 +0600

    Constructor overloading

---
+++ C-Sharp/ConstructorOverloading/Program.cs
@@ -0,0 +1,28 @@
+﻿class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+    
+    public Product(string name)
+    {
+        Name = name;
+    }
+
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Product product1 = new Product("iPhone 15 Pro Max", 1199);
+        Product product2= new Product("iPhone 15 Pro Max");
+        Console.WriteLine($"Product Name : {product1.Name} and Price {product1.Price}");
+        Console.WriteLine($"Product Name : {product1.Name}");
+    }
+}
