c966b7dc8991ab39c36a85c4dc230326dac85608
imamhossainroni <imamhossainroni95@gmail.com>
2024-08-18 11:48:34 +0600

    Upcasting and downcasting added

---
+++ C-Sharp/Casting/Program.cs
@@ -0,0 +1,30 @@
+﻿class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        // Dog dog = new Dog();
+        // Animal animal = dog; // Upcasting
+        // animal.Eat(); // This works
+      // animal.Bark(); // This won't work because Animal doesn't have the Bark method
+
+      Animal animal = new Animal(); // Upcasting
+      Dog dog = (Dog)animal;  // Downcasting
+      dog.Bark();  // Now we can call Bark, which isn't available in Animal
+
+    }
+}
