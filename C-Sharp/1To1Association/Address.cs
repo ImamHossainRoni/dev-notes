990cc94953906a22dddffd302f7c853bbe3b1924
imamhossainroni <imamhossainroni95@gmail.com>
2024-08-23 23:32:59 +0600

    Extra space removed

--- C-Sharp/1To1Association/Address.cs
+++ C-Sharp/1To1Association/Address.cs
@@ -3,13 +3,9 @@ namespace _1To1Association;
 public class Address
 {
     public string HouseNo { get; set; }
-    
     public string RoadNo { get; set; }
-    
     public string Area { get; set; }
-    
     public string PostCode { get; set; }
-    
     public string District { get; set; }
 
 
